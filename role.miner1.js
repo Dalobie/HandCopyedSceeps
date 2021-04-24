0cb50d77825aec3a3a63d688258ac8e0dd25b348
Dalobie <83100413+Dalobie@users.noreply.github.com>
2021-04-23 20:10:27 -0700

    Create role.miner1.js

---
+++ role.miner1.js
@@ -0,0 +1,25 @@
+var roleMiner1 = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        var source = Game.getObjectById('5bbcabba9099fc012e6342c6');
+        creep.moveTo(Game.flags.FlagMiner1)
+        
+        if(creep.memory.transfering && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.transfering = false;
+            creep.say('🔌 Harvesting   ');
+        }
+        if(!creep.memory.transfering && creep.store.getFreeCapacity() == 0) {
+            creep.memory.transfering = true;
+            creep.say('🔋 Transfering   ');
+        }
+        
+        if(!creep.memory.transfering) {
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            (creep.moveTo(Game.flags.FlagMiner1), {visualizePathStyle: {stroke: '#ffaa00'}});
+        }
+        }
+    }
+};
+
+module.exports = roleMiner1;
