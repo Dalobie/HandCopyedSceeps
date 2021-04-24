2b679b7f7fd7fa93b3f97cd4de1ac542c45e010f
Dalobie <83100413+Dalobie@users.noreply.github.com>
2021-04-23 20:11:53 -0700

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,7 +2,22 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
+        
+        var withdrawfrom = Game.getObjectById('608061833769c56803457f34');
+        /**
+        var container1 = Game.getObjectById('6080f91c84b96934256bf6fd');
+        var container2 = Game.getObjectById('608061833769c56803457f34');
+        
+        if(container2.store[RESOURCE_ENERGY] > (container2.store.getCapacity(RESOURCE_ENERGY)/2)) {
+            var withdrawfrom = container2;
+        }
+        else if(container1.store[RESOURCE_ENERGY] > (container1.store.getCapacity(RESOURCE_ENERGY)/2)) {
+            var withdrawfrom = container1;
+        }
+        else {
+            var withdrawfrom = container2;
+        }
+        **/
         if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
             creep.say('🔌 Harvesting   ');
@@ -18,9 +33,8 @@ var roleUpgrader = {
             }
         }
         else {
-            var source = Game.getObjectById('c99f0773646ccaf');
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.withdraw(withdrawfrom, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(withdrawfrom, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
 	}
