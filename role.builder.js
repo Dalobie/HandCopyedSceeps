5ca27adddaeda42f38085a2239a1d5a787b00329
Dalobie <83100413+Dalobie@users.noreply.github.com>
2021-04-28 13:54:11 -0700

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -2,56 +2,60 @@ var roleBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
-	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+        var container2 = Game.getObjectById('60851c59d4d94462fc69c451');
+        // set the 2 container vars
+        var buildsight = Game.getObjectById('6084810cf8671626737e8bc2');
+        
+        var donow = Game.getObjectById('608470c1f343dc4b6667a757');
+        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        
+        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
             creep.say('🔌 harvest');
 	    }
-	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-	        creep.memory.building = true;
-	        creep.say('🚧 build');
+        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+            creep.memory.building = true;
+            creep.say('🚧 build');
 	    }
-
-	    if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#9e9b00',opacity: 1}});
-                }
+        
+        if(!creep.memory.building) {
+            if(creep.withdraw(container2, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(container2, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
+	    }
+        
+        if(creep.memory.building) {
+            
+            
+            if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#9e9b00',opacity: 1}});
+            }
+            
             // upgrade if nothing to do
             if(targets.length == 0) {
             
-            if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔌 Harvesting');
-	    }
-	    if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
-	        creep.memory.upgrading = true;
-	        creep.say('⚡ upgradeing');
-	    }
-
-	    if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#9e9b00',opacity: 1}});
+                if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+                    creep.memory.upgrading = false;
+                     creep.say('🔌 Harvesting');
+	            }
+                if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+                   creep.memory.upgrading = true;
+                  creep.say('⚡ upgradeing');
+                }
+                
+                if(creep.memory.upgrading) {
+                    if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                       creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#9e9b00',opacity: 1}});
+                    }
+                }
+                else {
+                    var source = Game.getObjectById('608498ad80b5ea310857f370');
+                    if(creep.harvest(container2) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(container2, {visualizePathStyle: {stroke: '#ffaa00',opacity: 1}});
+                    }
+                }
             }
         }
-        else {
-            var source = Game.getObjectById('c99f0773646ccaf');
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00',opacity: 1}});
-            }
-        }
-            
-            }
-	    }
-	    else {
-	        var source = Game.getObjectById('c99f0773646ccaf');
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00',opacity: 1}});
-            }
-	    }
 	}
 };
-
 module.exports = roleBuilder;
