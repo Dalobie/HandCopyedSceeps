8a291087009e7efd286e00f44d116ca6a0655099
Dalobie <83100413+Dalobie@users.noreply.github.com>
2021-04-30 19:50:03 -0700

    Create role.gunner3.js

---
+++ role.gunner3.js
@@ -0,0 +1,29 @@
+var roleGunner3 = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        
+        if(!creep.memory.fighting) {
+            creep.memory.fighting = true;
+            creep.say('💂 Waiting   ');
+	    }
+	    if(creep.memory.fighting) {
+	        creep.memory.fighting = false;
+	        
+	    }
+        
+        if(!creep.memory.fighting) {
+            var target = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
+            
+            if(target.length > 0) {
+                creep.say('🔫'+'PEWPEW');
+                creep.rangedAttack(target[0]);
+            }
+        }
+        if(!creep.memory.fighting) {
+        creep.moveTo(Game.flags.FlagGunner3);
+        }
+    }
+};
+
+module.exports = roleGunner3;
